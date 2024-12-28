91d71852ca57a7e0b56d4654f2357484f3f43bd0
tungngk <tungngk@mz.co.kr>
2024-12-28 11:11:26 +0700

    add files

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const config = {
-  MicrosoftAppId: process.env.BOT_ID,
+  MicrosoftAppId: "ad27924c-3925-4eb5-9aa8-0a822cb608b0",
   MicrosoftAppType: process.env.BOT_TYPE,
   MicrosoftAppTenantId: process.env.BOT_TENANT_ID,
   MicrosoftAppPassword: process.env.BOT_PASSWORD,
